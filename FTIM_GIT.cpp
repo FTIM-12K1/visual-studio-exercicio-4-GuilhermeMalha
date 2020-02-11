0a7be552de80a488d04fabd11a42756ee5bf02f9
GuilhermeMalha <60393938+GuilhermeMalha@users.noreply.github.com>
2020-02-11 14:51:38 +0000

    Add files via upload

---
+++ FTIM_GIT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int valor1;
+	int valor2;
+	cout << "Introduza um valor para somar: " << endl;
+	cin >> valor1;
+	cout << "Introduza um segundo valor para somar: " << endl;
+	cin >> valor2;
+
+	cout << "A soma: " << valor1 + valor2 << "." << endl;
+}
+
